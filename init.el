8d06d8d5ac769f7035f5a909946e8fcf6919eb11
lest <just.lest@gmail.com>
2010-08-02 22:54:38 +0300

    modify ido ignore buffers

--- init.el
+++ init.el
@@ -51,7 +51,8 @@
                            "^\*compilation"
                            "^\*rake"
                            "^\*generate"
-                           "^\*magit"))
+                           "^\*magit"
+                           "^\*rhtml-ruby-hook"))
 
 ;; delete trailing whitespace on save
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
