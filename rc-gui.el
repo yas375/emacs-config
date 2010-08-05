1aae65707ba76d3bc9b9c5fe6fb05cec4fe7dab8
yas <ilyukevich.victor@gmail.com>
2010-08-05 17:11:05 +0300

    Revert "enable colors as in textmate"
    This reverts commit a482342a5e252f353830a366ef8369a55c4b18c2.

--- rc-gui.el
+++ rc-gui.el
@@ -27,16 +27,16 @@
  '(org-hide ((((background light)) (:foreground "grey90")))))
 
 ;; TextMate idleFingers
-(custom-set-faces
- '(default ((t (:background "#282828" :foreground "#EEEEEE")))) ;#353535
- '(region ((t (:background "#535B72")))) ;#656E87
- '(highlight ((t (:background "#353637")))) ;#454647
- '(font-lock-comment-face ((t (:foreground "#BC9458")))) ;#CEA564
- '(font-lock-string-face ((t (:foreground "#A5C361")))) ;#ABD16A
- '(font-lock-keyword-face ((t (:foreground "#DA802B")))) ;#E28833
- '(font-lock-constant-face((t (:foreground "#6C99BC")))) ;#74AACA
- '(font-lock-builtin-face ((t (:foreground "#6C99BC"))))
- '(font-lock-type-face ((t (:foreground "#CCCC33")))) ;#D3D917
- '(font-lock-function-name-face ((t (:foreground "#FFF980")))) ;#FEFD86
- '(font-lock-variable-name-face ((t (:foreground "#B7DFF8")))) ;#BBE6FB
-)
+;; (custom-set-faces
+;;  '(default ((t (:background "#282828" :foreground "#EEEEEE")))) ;#353535
+;;  '(region ((t (:background "#535B72")))) ;#656E87
+;;  '(highlight ((t (:background "#353637")))) ;#454647
+;;  '(font-lock-comment-face ((t (:foreground "#BC9458")))) ;#CEA564
+;;  '(font-lock-string-face ((t (:foreground "#A5C361")))) ;#ABD16A
+;;  '(font-lock-keyword-face ((t (:foreground "#DA802B")))) ;#E28833
+;;  '(font-lock-constant-face((t (:foreground "#6C99BC")))) ;#74AACA
+;;  '(font-lock-builtin-face ((t (:foreground "#6C99BC"))))
+;;  '(font-lock-type-face ((t (:foreground "#CCCC33")))) ;#D3D917
+;;  '(font-lock-function-name-face ((t (:foreground "#FFF980")))) ;#FEFD86
+;;  '(font-lock-variable-name-face ((t (:foreground "#B7DFF8")))) ;#BBE6FB
+;; )
