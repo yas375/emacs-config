792e535d5ecb485dc96992b8965cac70f46c3f49
alexeis <alexeis@alexeis.localdomain>
2010-08-17 11:54:55 +0300

    added php-mode

--- init.el
+++ init.el
@@ -142,6 +142,10 @@
 ;; use rvm default ruby
 (rvm-use-default)
 
+;; php mode
+(autoload 'php-mode "php-mode" "Major mode for editing php code." t)
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+
 ;; start server
 (require 'server)
 (when (not (server-running-p))
