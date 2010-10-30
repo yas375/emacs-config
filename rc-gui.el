04c5ad4e2ba572dd62e401debf464683ef6de925
yas <ilyukevich.victor@gmail.com>
2010-10-30 10:54:14 +0300

    remove custom cursor settings

--- rc-gui.el
+++ rc-gui.el
@@ -44,31 +44,3 @@
 ;;  '(font-lock-function-name-face ((t (:foreground "#FFF980")))) ;#FEFD86
 ;;  '(font-lock-variable-name-face ((t (:foreground "#B7DFF8")))) ;#BBE6FB
 ;; )
-
-;; Change cursor color according to mode; inspired by
-;; http://www.emacswiki.org/emacs/ChangingCursorDynamically
-(setq djcb-read-only-color       "gray")
-;; valid values are t, nil, box, hollow, bar, (bar . WIDTH), hbar,
-;; (hbar. HEIGHT); see the docs for set-cursor-type
-
-(setq djcb-read-only-cursor-type 'hbar)
-(setq djcb-overwrite-color       "red")
-(setq djcb-overwrite-cursor-type 'box)
-(setq djcb-normal-color          "blue")
-(setq djcb-normal-cursor-type    'bar)
-
-(defun djcb-set-cursor-according-to-mode ()
-  "change cursor color and type according to some minor modes."
-
-  (cond
-    (buffer-read-only
-      (set-cursor-color djcb-read-only-color)
-      (setq cursor-type djcb-read-only-cursor-type))
-    (overwrite-mode
-      (set-cursor-color djcb-overwrite-color)
-      (setq cursor-type djcb-overwrite-cursor-type))
-    (t
-      (set-cursor-color djcb-normal-color)
-      (setq cursor-type djcb-normal-cursor-type))))
-
-(add-hook 'post-command-hook 'djcb-set-cursor-according-to-mode)
